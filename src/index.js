336609cce4ed5b0966d499726fffc2b9fb453654
Momo Rao <monicaxrao@gmail.com>
2018-10-29 16:29:06 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import storeFactory from './store'
+import { suggestResortNames } from './actions'
+
+const store = storeFactory()
+
+store.dispatch(
+	suggestResortNames('hea')
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
